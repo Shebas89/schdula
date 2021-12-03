3105a80fc601f5fa9236ed208ba81080ad7c379b
Sebastian Velandia <sebastian.velandia@blackboard.com>
2021-12-02 22:52:03 -0500

    changing host to AWS

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
 export default {
   PORT: process.env.PORT || 3001,
-  DB_HOST: process.env.DB_HOST || 'localhost',
-  DB_USER: process.env.DB_USER || 'root',
-  DB_PASSWORD: process.env.DB_PASSWORD || 'password',
-  DATABASE: process.env.DATABASE || 'db-schedula',
-  DB_PORT: process.env.DB_PORT || 3308
+  DB_HOST: process.env.DB_HOST || 'database-schedula.cn9eiu9utwsi.us-east-1.rds.amazonaws.com',
+  DB_USER: process.env.DB_USER || 'admin',
+  DB_PASSWORD: process.env.DB_PASSWORD || 'SchedulA',
+  DATABASE: process.env.DATABASE || 'db_schedula',
+  DB_PORT: process.env.DB_PORT || 3306
 }
